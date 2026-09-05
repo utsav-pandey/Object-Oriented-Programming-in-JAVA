5bbc0a0742ca3a44ba4b0ad3b90a9a367694d310
utsav-pandey <utsavpandey107@gmail.com>
2026-09-05 13:10:58 +0530

    Added the first file

---
+++ first.java
@@ -0,0 +1,24 @@
+/*
+    * Java is a platform-independent, object-oriented programming language.
+    * It is strongly typed, compiled and then interpreted.
+    * it is multithreaded, secure and robust programming language.
+*/
+/*
+    * Java is a high-level programming language developed by Sun Microsystems.
+    * It was originally designed for interactive television, but it was too advanced for the digital cable television industry at the time.
+    * The language was initially called Oak after an oak tree that stood outside Gosling's office.
+    * Later the project went by the name Green and was finally renamed Java, from Java coffee, a type of coffee from Indonesia.
+    * It offers long term support.
+*/
+
+/*
+    * JDK:- it stands for java development kit. It has java (compiler)(java --version) to compile the code to byte code.
+    * it also has JRE (java runtime environment) to run the byte code i.e (javac) (javac -version)
+    * We can use any integrated development environment (IDE) to write java code like eclipse, netbeans, intellij etc. but we prefer intellij or vscode.
+*/
+
+public class first {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!\n");
+    }
+}
